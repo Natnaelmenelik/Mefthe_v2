08f27b964d169ee6fc1bf98d2c21c7806bcd3d61
NatnaelMenelik <natnaelmenelik3@gmail.com>
2025-05-28 16:08:32 +0300

    Typography Changes

--- layouts/Home/Hero.tsx
+++ layouts/Home/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
       <section className="mt-8 p-4">
         <div className="max-w-md mx-auto sm:max-w-xl md:max-w-3xl lg:max-w-4xl xl:max-w-6xl 2xl:max-w-[1350px]">
           <div className="grid gap-5 md:grid-cols-2">
-            <div className="md:mt-10 space-y-5">
+            <div className="md:mt-10 xl:md:mt-20 space-y-5">
               <h1 className="text-3xl font-bold text-center md:text-start md:text-4xl xl:text-5xl">
                 <span className="text-primary">{t("titleStart")}</span>
                 {t("titleEnd")}
